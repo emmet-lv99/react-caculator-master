4c69d963d731633ebab8d21a2e3c8e78ffdbb70e
dachanisfull@gmail.com <dachan.kim>
2021-12-17 20:50:34 +0900

    컴포넌트 간 연결

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
-const Interface = () => {
+const Interface = props => {
+  const onClickNumber = num => {
+    if (props.emitInterfaceVal) props.emitInterfaceVal(num)
+  }
+  const onClickOperator = op => {
+    if (props.emitInterfaceOp) props.emitInterfaceOp(op)
+  }
   return (
     <>
       <div>
@@ -8,25 +14,116 @@ const Interface = () => {
           <button className="interface-item">C</button>
           <button className="interface-item">.</button>
           <button className="interface-item">←</button>
-          <button className="interface-item interface-item-operator">÷</button>
+          <button
+            onClick={() => {
+              onClickOperator('/')
+            }}
+            className="interface-item interface-item-operator"
+          >
+            ÷
+          </button>
         </div>
         <div className="interface-row">
-          <button className="interface-item">7</button>
-          <button className="interface-item">8</button>
-          <button className="interface-item">9</button>
-          <button className="interface-item interface-item-operator">×</button>
+          <button
+            onClick={() => {
+              onClickNumber(7)
+            }}
+            className="interface-item"
+          >
+            7
+          </button>
+          <button
+            onClick={() => {
+              onClickNumber(8)
+            }}
+            className="interface-item"
+          >
+            8
+          </button>
+          <button
+            onClick={() => {
+              onClickNumber(9)
+            }}
+            className="interface-item"
+          >
+            9
+          </button>
+          <button
+            onClick={() => {
+              onClickOperator('x')
+            }}
+            className="interface-item interface-item-operator"
+          >
+            ×
+          </button>
         </div>
         <div className="interface-row">
-          <button className="interface-item">4</button>
-          <button className="interface-item">5</button>
-          <button className="interface-item">6</button>
-          <button className="interface-item interface-item-operator">－</button>
+          <button
+            onClick={() => {
+              onClickNumber(4)
+            }}
+            className="interface-item"
+          >
+            4
+          </button>
+          <button
+            onClick={() => {
+              onClickNumber(5)
+            }}
+            className="interface-item"
+          >
+            5
+          </button>
+          <button
+            onClick={() => {
+              onClickNumber(6)
+            }}
+            className="interface-item"
+          >
+            6
+          </button>
+          <button
+            onClick={() => {
+              onClickOperator('-')
+            }}
+            className="interface-item interface-item-operator"
+          >
+            －
+          </button>
         </div>
         <div className="interface-row">
-          <button className="interface-item">1</button>
-          <button className="interface-item">2</button>
-          <button className="interface-item">3</button>
-          <button className="interface-item interface-item-operator">＋</button>
+          <button
+            onClick={() => {
+              onClickNumber(1)
+            }}
+            className="interface-item"
+          >
+            1
+          </button>
+          <button
+            onClick={() => {
+              onClickNumber(2)
+            }}
+            className="interface-item"
+          >
+            2
+          </button>
+          <button
+            onClick={() => {
+              onClickNumber(3)
+            }}
+            className="interface-item"
+          >
+            3
+          </button>
+          <button
+            onClick={() => {
+              onClickOperator('+')
+            }}
+            className="interface-item interface-item-operator"
+          >
+            ＋
+          </button>
         </div>
         <div className="interface-row">
           <button className="interface-item">0</button>
--- src/views/Main.js
+++ src/views/Main.js
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import Display from '../components/Display'
 import Result from '../components/Result'
 import Interface from '../components/Interface'
 import '../Calculator.css'
 
+/*Todo 변수 구조 세팅
+ *Todo interface에서 버튼 별로 값 넘겨 받기
+ *
+ */
+
 const Main = () => {
+  const [result, setResult] = useState(0)
+  const [display, setDisplay] = useState('')
+  const [operator, setOperator] = useState('')
+  const [value1, setValue1] = useState(0)
+  const [value2, setValue2] = useState(0)
+
+  const emitInterfaceVal = num => {
+    console.log(num)
+  }
+
+  const emitInterfaceOp = op => {
+    console.log(op)
+  }
+
   return (
     <>
       <header>
         <Header />
       </header>
       <main className="calculator-wrapper">
-        <Display />
-        <Result />
-        <Interface />
+        <Display display={display} />
+        <Result result={result} />
+        <Interface
+          emitInterfaceVal={emitInterfaceVal}
+          emitInterfaceOp={emitInterfaceOp}
+        />
       </main>
     </>
   )
